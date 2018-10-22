ad18626043cbfb6f6a673d06b68591f029f4b43b
Linpn <lpn5200@qq.com>
2018-10-22 15:52:30 +0800

    Zookeeper configuration center

--- zkconfter/src/com/github/linpn/zkconfter/config/PropertyPlaceholderConfigurer.java
+++ zkconfter/src/com/github/linpn/zkconfter/config/PropertyPlaceholderConfigurer.java
@@ -56,6 +56,7 @@ public class PropertyPlaceholderConfigurer extends
             } else {
                 if (location.startsWith("file://") || location.startsWith("classpath:") || location.startsWith("classpath*:")) {
                 } else {
+                    location.replaceFirst("\\/", "");
                     location = contextPath + location;
                 }
             }
